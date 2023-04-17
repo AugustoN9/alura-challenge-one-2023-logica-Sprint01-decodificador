f15220e535dd693a16c014d3812847573e0a2242
Augusto Lopez <augusto.lopez@estudante.senairs.edu.br>
2023-04-17 01:43:16 -0300

    subindo projeto...

---
+++ js/script.js
@@ -0,0 +1,88 @@
+
+var encodedString;
+
+function encrypt() {
+
+    var baseString = document.querySelector("#texto").value;
+    encodedString = baseString.replace(/a/g, "c1");
+    encodedString = encodedString.replace(/e/g, "c2");
+    encodedString = encodedString.replace(/i/g, "c3");
+    encodedString = encodedString.replace(/o/g, "c4");
+    encodedString = encodedString.replace(/u/g, "c5");
+    var baseString2 = encodedString.replace(/c1/g, "ai");
+    baseString2 = baseString2.replace(/c2/g, "enter");
+    baseString2 = baseString2.replace(/c3/g, "imes");
+    baseString2 = baseString2.replace(/c4/g, "ober");
+    baseString2 = baseString2.replace(/c5/g, "ufat");
+
+    encodedString = baseString2;
+
+    document.getElementById("texto").value = encodedString;
+}
+
+var decodedString;
+
+function decrypt() {
+
+    var baseString = document.getElementById("texto").value;
+    decodedString = baseString.replace(/ai/g, "c1");
+    decodedString = decodedString.replace(/enter/g, "c2");
+    decodedString = decodedString.replace(/imes/g, "c3");
+    decodedString = decodedString.replace(/ober/g, "c4");
+    decodedString = decodedString.replace(/ufat/g, "c5");
+    var baseString2 = decodedString.replace(/c1/g, "a");
+    baseString2 = baseString2.replace(/c2/g, "e");
+    baseString2 = baseString2.replace(/c3/g, "i");
+    baseString2 = baseString2.replace(/c4/g, "o");
+    baseString2 = baseString2.replace(/c5/g, "u");
+
+    decodedString = baseString2;
+
+    document.getElementById("texto").value = decodedString;
+}
+
+
+/*
+var tituloBtn = document.getElementById("btn1").value;
+
+tituloBtn.addEventListener('click', updateButton);
+
+function updateButton() {
+
+    if (tituloBtn === "Criptografar") {
+        tituloBtn = "Limpar";
+
+    } else {
+        tituloBtn = "Criptografar";
+    }
+}
+
+function limpa(campo) {
+    document.getElementById(campo).value = '';
+}
+
+function limpatudo() {
+    var input = document.getElementById("texto").value;
+    if (input != '') {
+        document.getElementById("texto").value = '';
+    }
+    var input = document.getElementById("texto").value;
+    if (input != '') {
+        document.getElementById("texto").value = '';
+    }
+    var input = document.getElementById("texto").value;
+    if (input != '') {
+        document.getElementById("texto").value = '';
+    }
+    var input = document.getElementById("texto").value;
+    if (input != '') {
+        document.getElementById("texto").value = '';
+    }
+}
+*/
+
+function copiar() {
+    var copy = document.getElementById("texto").value;
+    document.getElementById("textcopy").value = copy;
+    //limpa("texto");
+}
